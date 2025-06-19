f2e090335af3f45262ce3c07c13628c8b4460484
seuthootDev <seuthootdev@gmail.com>
2025-06-19 20:57:53 +0900

    0619

--- nestjs-board-app/src/app.module.ts
+++ nestjs-board-app/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
+    ConfigModule.forRoot({
+      isGlobal: true, // 모든 모듈에서 환경변수를 사용할 수 있도록
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => typeOrmConfig(configService),
+      inject: [ConfigService],
+    }),
     BoardsModule,
   ],
 })
--- nestjs-board-app/src/configs/typeorm.config.ts
+++ nestjs-board-app/src/configs/typeorm.config.ts
@@ -1,11 +1,12 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { ConfigService } from "@nestjs/config";
 
-export const typeOrmConfig : TypeOrmModuleOptions = {
+export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
     type: 'postgres',
-    url: 'postgresql://postgres.fyxacgunrakumvzuawwt:wjdtmdgns1!@aws-0-ap-northeast-2.pooler.supabase.com:5432/postgres',
+    url: `postgresql://postgres.fyxacgunrakumvzuawwt:${configService.get('PASSWORD')}@aws-0-ap-northeast-2.pooler.supabase.com:5432/postgres`,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     ssl: {
         rejectUnauthorized: false
     },
     synchronize: true
-}
+});
